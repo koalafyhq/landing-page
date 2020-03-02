001c1d0f0aa7859d69b3b899d3428c37a360e444
Fariz Rizaldy <farizrizaldy@icloud.com>
2020-03-02 19:43:22 +0700

    feat: add hero to index page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
-function IndexPage() {
-  return <h1>It works!</h1>
+import Hero from '~/components/Hero'
+
+const IndexPage = () => {
+  return (
+    <main>
+      <Hero
+        title='JAMStack done right'
+        description='build blazing fast & secure website to leveraging your business
+        and digital transformation'
+        ctaButtonTitle='Work with us'
+        ctaButtonLink='/contact'
+      />
+    </main>
+  )
 }
 
 export default IndexPage
