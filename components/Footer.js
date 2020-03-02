7882c60cec37d7f308134e2bdb75e976d87e5ab1
Fariz Rizaldy <farizrizaldy@icloud.com>
2020-03-02 19:44:11 +0700

    refactor: convert function to const for consistency

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,14 @@
-function Footer() {
-  return (
-    <footer>
-      <p>Footer</p>
-      <style jsx>{`
-        footer {
-          padding: 10px;
-          background-color: #000;
-          color: #fff;
-        }
-      `}</style>
-    </footer>
-  )
-}
+const Footer = () => (
+  <footer>
+    <p>Footer</p>
+    <style jsx>{`
+      footer {
+        padding: 10px;
+        background-color: #000;
+        color: #fff;
+      }
+    `}</style>
+  </footer>
+)
 
 export default Footer
