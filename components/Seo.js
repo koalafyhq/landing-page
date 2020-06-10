13eea82fb12be40899108d4c3bd1d0274fb01562
Fariz Rizaldy <farizrizaldy@icloud.com>
2020-06-10 16:07:52 +0700

    fix: use next/head and adding some default values

--- components/Seo.js
+++ components/Seo.js
@@ -1,51 +1,59 @@
-const Seo = ({ title, description, canonical }) => {
+import Head from 'next/head'
+import { defaultOgImage, baseURL } from '~/config'
+
+const Seo = ({
+  title,
+  description,
+  type = 'Article',
+  shareImage,
+  canonical
+}) => {
+  const ogImage = shareImage || defaultOgImage
   const jsonLd = {
     '@context': `https://schema.org/`,
     '@type': type,
     url: canonical,
-    image: shareImage
-      ? {
-          '@type': `ImageObject`,
-          url: shareImage,
-          width: 1000,
-          height: 523
-        }
-      : undefined,
+    image: {
+      '@type': `ImageObject`,
+      url: ogImage,
+      width: 1000,
+      height: 523
+    },
     publisher: {
       '@type': `Organization`,
-      name: settings.title,
+      name: title,
       logo: {
         '@type': `ImageObject`,
-        url: publisherLogo,
+        url: ogImage,
         width: 60,
         height: 60
       }
     },
     mainEntityOfPage: {
       '@type': `WebPage`,
-      '@id': `https://koalafyhq.com`
+      '@id': baseURL
     },
     description
   }
+
   return (
-    <>
-      <Helmet>
-        <title>{title}</title>
-        <meta name='description' content={description} />
-        <link rel='canonical' href={canonical} />
-        <meta property='og:site_name' content={settings.title} />
-        <meta property='og:type' content='website' />
-        <meta property='og:title' content={title} />
-        <meta property='og:description' content={description} />
-        <meta property='og:url' content={canonical} />
-        <meta name='twitter:title' content={title} />
-        <meta name='twitter:description' content={description} />
-        <meta name='twitter:url' content={canonical} />
-        <script type='application/ld+json'>
-          {JSON.stringify(jsonLd, undefined, 4)}
-        </script>
-      </Helmet>
-      <ImageMeta image={shareImage} />
-    </>
+    <Head>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <link rel='canonical' href={canonical} />
+      <meta property='og:site_name' content={title} />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={description} />
+      <meta property='og:url' content={canonical} />
+      <meta name='twitter:title' content={title} />
+      <meta name='twitter:description' content={description} />
+      <meta name='twitter:url' content={canonical} />
+      <script type='application/ld+json'>
+        {JSON.stringify(jsonLd, undefined, 4)}
+      </script>
+    </Head>
   )
 }
+
+export default Seo
